125a3a61891f9de524572b0daf7b893b20a3c85e
torph <73564623+Torphedo@users.noreply.github.com>
2025-09-22 18:10:20 -0400

    Lots of cleanup & minor fixes

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
@@ -6,40 +7,32 @@
 #include <sys/stat.h>
 
 #include <common/logging.h>
+#include <common/file.h>
 #include <common/image.h>
+#include <common/path.h>
 #include "dds.h"
 
 typedef enum {
-	HGHT = 0,
-	DDS = 1
+	HGHT,
+	DDS,
+	INVALID,
 }input_type;
-
-// Get size of a file using POSIX stat() function.
-uint32_t filesize(const char* path) {
-	struct stat st = {0};
-	stat(path, &st);
-	return st.st_size;
-}
-
 
 void dds_to_hght(const char* dds_path, const char* hght_path) {
 	// Heightmaps are always 256x256 16-bit numbers
 	uint16_t hght_data[256*256] = {0};
-	uint32_t hght_size = sizeof(hght_data);
+	const u32 hght_size = sizeof(hght_data);
 
 	// Sanity check that our file is the right size before we do anything
-	uint32_t expected_size = sizeof(dds_header) + hght_size;
-	if (filesize(dds_path) != expected_size) {
+	const u32 expected_size = sizeof(dds_header) + hght_size;
+	if (file_size(dds_path) != expected_size) {
 		LOG_MSG(error, "Invalid DDS file (wrong size)\n");
 		return;
 	}
 
-	// Try to open our HGHT input & DDS output files
 	FILE* hght = fopen(hght_path, "wb");
 	FILE* dds = fopen(dds_path, "rb");
-
-	// Bail if we can't open the files
-	if (hght == NULL || dds == NULL) {
+	if (!hght || !dds) {
 		fclose(hght);
 		fclose(dds);
 		return;
@@ -48,36 +41,32 @@ void dds_to_hght(const char* dds_path, const char* hght_path) {
 	// Skip over the DDS header, then read heightmap into memory
 	fseek(dds, sizeof(dds_header), SEEK_SET);
 	fread(&hght_data, hght_size, 1, dds);
-	fclose(dds); // Close input file
+	fclose(dds);
 
-	// Write the heightmap data to our output.
 	fwrite(&hght_data, hght_size, 1, hght);
 	fclose(hght);
 }
 
 void hght_to_dds(const char* hght_path, const char* dds_path) {
 	// Heightmaps are always 256x256 16-bit numbers
-	uint16_t hght_data[256*256] = {0};
-	uint32_t hght_size = sizeof(hght_data);
+	u16 hght_data[256*256] = {0};
+	const u32 hght_size = sizeof(hght_data);
 
 	// Sanity check that our file is the right size before we do anything
-	if (filesize(hght_path) != hght_size) {
+	if (file_size(hght_path) != hght_size) {
 		LOG_MSG(error, "Invalid HGHT file (wrong size)\n");
 		return;
 	}
 
-	// Try to open our HGHT input & DDS output files
 	FILE* hght = fopen(hght_path, "rb");
-
-	// Bail if we can't open the files
-	if (hght == NULL) {
+	if (!hght) {
 		fclose(hght);
 		return;
 	}
 
 	// Read heightmap into memory
 	fread(hght_data, hght_size, 1, hght);
-	fclose(hght); // Close input file
+	fclose(hght);
 
 	// Write the image header to our output, then the heightmap data
 	// (which will be interpreted as pixels).
@@ -98,50 +87,46 @@ int main(int argc, char** argv) {
 	if (argc == 1) {
 		LOG_MSG(error, "Not enough arguments.\n");
 		LOG_MSG(info, "Usage: hght [path to HGHT/DDS file]\n");
-		return 1;
+        return EXIT_FAILURE;
 	}
 
-	char last_char = input_path[strlen(input_path) - 1];
-	input_type filetype = (last_char == 's');
-	if (filetype == DDS) {
+	input_type filetype = INVALID;
+    if (path_has_extension(input_path, ".dds")) {
+        filetype = DDS;
 		LOG_MSG(info, "Converting DDS file to HGHT.\n");
-	}
-	else if (last_char != 't') {
-		LOG_MSG(error, "Provided file is not HGHT or DDS.\n");
-	}
-	else {
+    }
+    else if (path_has_extension(input_path, ".hght")) {
+        filetype = HGHT;
 		LOG_MSG(info, "Converting HGHT file to DDS.\n");
-	}
+    } else {
+		LOG_MSG(error, "Provided file is not HGHT or DDS.\n");
+        return EXIT_FAILURE;
+    }
 
 	// Get paths fom user input. If no dds output is given, use the HGHT path
 	// and append ".dds". ("test.hght" outputs "test.hght.dds")
 	char* out_path = NULL;
 	if (argc >= 3) {
 		out_path = argv[2];
-	}
-	else {
+	} else {
 		// Allocate for output path
-		out_path = malloc(strlen(input_path) + sizeof(".dds"));
-		if (out_path == NULL) {
-			LOG_MSG(error, "Failed to allocate memory for dds path.\n");
-			return 1;
+        const u32 out_path_size = strlen(input_path) + sizeof(".hght");
+		out_path = malloc(out_path_size);
+		if (!out_path) {
+			LOG_MSG(error, "Failed to allocate for dds path.\n");
+			return EXIT_FAILURE;
 		}
-		// Copy the input path, then add file extension of the output format to the end
-		strcpy(out_path, input_path);
-		if (filetype == HGHT) {
-			strcat(out_path, ".dds");
-		}
-		else {
-			strcat(out_path, ".hght");
-		}
+
+        const char* extension = (filetype == HGHT) ? "hght" : "dds";
+        snprintf(out_path, out_path_size, "%s.%s", input_path, extension);
 	}
 
 	if (filetype == HGHT) {
 		hght_to_dds(input_path, out_path);
-	}
-	else {
+	} else {
 		dds_to_hght(input_path, out_path);
 	}
 
 	free(out_path);
+    return EXIT_SUCCESS;
 }
