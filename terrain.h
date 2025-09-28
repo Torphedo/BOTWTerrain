a7586382e1783880ab0417d516c92e3cfab25c26
torph <73564623+Torphedo@users.noreply.github.com>
2025-09-28 13:20:34 -0400

    Add MATE struct

--- terrain.h
+++ terrain.h
@@ -6,6 +6,7 @@
 // https://zeldamods.org/wiki/Category:File_extensions_(STERA)
 // https://zeldamods.org/wiki/Water.extm
 // https://zeldamods.org/wiki/HGHT
+// https://zeldamods.org/wiki/MATE
 
 #include <stdbool.h>
 #include <assert.h>
@@ -13,21 +14,18 @@
 
 enum {
     HGHT_WIDTH = 256,
+    MATE_WIDTH = HGHT_WIDTH,
     WATER_EXTM_WIDTH = 64,
     WATER_NO_FLOW = 128,
 };
+
+// Used to convert world coords to Z order curve index for HGHT
+#define MAP_SIZE (6000.0f)
 
 typedef struct {
     u16 height[HGHT_WIDTH][HGHT_WIDTH];
 }hght_t;
 static_assert(sizeof(hght_t) == 2 * 256 * 256, "HGHT struct size is wrong!");
-
-#define MAP_SIZE (6000.0f)
-
-bool hght_to_dds(const char* inpath, const char* outpath);
-bool dds_to_hght(const char* inpath, const char* outpath);
-
-s32 pos_to_zorder_idx(u8 detail_lvl, float x, float y);
 
 // https://zeldamods.org/wiki/Water.extm
 typedef struct {
@@ -42,8 +40,27 @@ static_assert(sizeof(water_vert) == 8, "water.extm vertex size is wrong!");
 typedef struct {
     water_vert vertices[WATER_EXTM_WIDTH][WATER_EXTM_WIDTH];
 }water_extm;
+static_assert(sizeof(water_extm) == 8 * 64 * 64, "water.extm size is wrong!");
+
+// https://zeldamods.org/wiki/MATE
+typedef struct {
+    u8 material0;
+    u8 material1;
+    u8 blend_weight;
+    u8 unknown;
+}mate_vert;
+static_assert(sizeof(mate_vert) == 4, "Material vertex size is wrong!");
+
+typedef struct {
+    mate_vert vertices[MATE_WIDTH][MATE_WIDTH];
+}mate_t;
+static_assert(sizeof(mate_t) == 256 * 256 * 4, "Material map size is wrong!");
+
+bool hght_to_dds(const char* inpath, const char* outpath);
+bool dds_to_hght(const char* inpath, const char* outpath);
 
 bool water_to_dds(const char* inpath, const char* outpath);
 bool dds_to_water(const char* inpath, const char* outpath);
 
-static_assert(sizeof(water_extm) == 8 * 64 * 64, "water.extm size is wrong!");
+s32 pos_to_zorder_idx(u8 detail_lvl, float x, float y);
+
